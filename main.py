14862f92b87297b1e2785b7e4edffa63717e0ac8
Reenabahen chauhan <reenabahenchauhan1990@gmail.com>
2025-09-27 17:01:16 +0100

    add Mul

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ def Add(a,b):
 def sub(a,b):
     return a -b
 
+def mul(a,b):
+    return a * b
+
 if __name__ == "__main__":
     print("Add",Add(5,3))
+    print("Sub",sub(5,3))
+    print("Mul",mul(5,3))
